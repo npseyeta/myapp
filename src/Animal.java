04593d95f9ee31612c086084918db190abb44c7f
-g <-g>
2020-09-23 20:51:00 +0700

    bring back everything

--- src/Animal.java
+++ src/Animal.java
@@ -1,7 +1,11 @@
 public class Animal {
+<<<<<<< HEAD
     private String name;
 
     public Animal (String name) {
         this.name = name;
     }
+=======
+    
+>>>>>>> fe14598... Add constructer in Animal.java
 }
